52b76e39d29ad9497ae8181c3dbfd2baef10d05b
aimiy <mioly@foxmail.com>
2018-01-26 17:48:39 +0800

    weixin

--- web/fire/build/main.js
+++ web/fire/build/main.js
@@ -65,7 +65,7 @@ module.exports =
 /******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 11);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -84,7 +84,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: ['~assets/css/main.css'],
+  css: ['~static/css/main.css'],
   /*
   ** Customize the progress-bar color
   */
@@ -111,77 +111,187 @@ module.exports = {
 
 /***/ },
 /* 1 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
-module.exports = __webpack_require__(4);
-
+module.exports = require("path");
 
 /***/ },
 /* 2 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-module.exports = require("koa");
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_path__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_path___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_path__);
+
+
+
+var host = process.env.HOST || 'localhost';
+var env = "development" || 'development';
+var conf = !(function webpackMissingModule() { var e = new Error("Cannot find module \".\""); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+/* harmony default export */ exports["a"] = __WEBPACK_IMPORTED_MODULE_0_lodash___default.a.assign({
+  env: env,
+  host: host
+}, conf);
 
 /***/ },
 /* 3 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-module.exports = require("nuxt");
+module.exports = __webpack_require__(10);
+
 
 /***/ },
 /* 4 */
 /***/ function(module, exports) {
 
-module.exports = require("regenerator-runtime");
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 4;
+
 
 /***/ },
 /* 5 */
+/***/ function(module, exports) {
+
+module.exports = require("koa");
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+module.exports = require("nuxt");
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+module.exports = require("ramda");
+
+/***/ },
+/* 8 */
+/***/ function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 8;
+
+
+/***/ },
+/* 9 */
+/***/ function(module, exports) {
+
+module.exports = require("lodash");
+
+/***/ },
+/* 10 */
+/***/ function(module, exports) {
+
+module.exports = require("regenerator-runtime");
+
+/***/ },
+/* 11 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_E_learn_vue_weixin_webapp_web_fire_node_modules_babel_runtime_regenerator__ = __webpack_require__(1);
+/* WEBPACK VAR INJECTION */(function(__dirname) {Object.defineProperty(exports, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_E_learn_vue_weixin_webapp_web_fire_node_modules_babel_runtime_regenerator__ = __webpack_require__(3);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_E_learn_vue_weixin_webapp_web_fire_node_modules_babel_runtime_regenerator___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_E_learn_vue_weixin_webapp_web_fire_node_modules_babel_runtime_regenerator__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa__ = __webpack_require__(5);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_koa___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_koa__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_nuxt__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_nuxt___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_nuxt__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__config__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_path__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_path___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_path__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_nuxt__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_nuxt___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_nuxt__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__nuxt_config__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__nuxt_config___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5__nuxt_config__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_ramda__ = __webpack_require__(7);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_ramda___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_ramda__);
 
+
+var _this = this;
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+
+
+
+
+
+// 函数式风格，数据不变性和函数无副作用是它的核心设计理念，结合它的函数自动柯里化可以让多个函数的排
+// 列和数据的变换传递变得更容易
+
+
+var r = function r(path) {
+  return __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_3_path__["resolve"])(__dirname, path);
+};
+var MIDDLEWARE = ['database', 'general', 'router'];
+var app = new __WEBPACK_IMPORTED_MODULE_1_koa___default.a();
+
+var useMiddleware = function useMiddleware(app) {
+  // 中间件的个数不定，通过 Ramda 的特性，从右往左进行函数组合，右侧函数的返回结果总是左侧函数的输入参数
+  // R.map(console.log)([1, 2, 3])
+  // MIDDLEWARE 数组交给了 R.map
+  // 分别拿到的单个数组中的值，我们可以通过 R.compose 再次进行组合。
+  return __WEBPACK_IMPORTED_MODULE_6_ramda___default.a.map(__WEBPACK_IMPORTED_MODULE_6_ramda___default.a.compose(__WEBPACK_IMPORTED_MODULE_6_ramda___default.a.map(function (i) {
+    return i(app);
+  }), !(function webpackMissingModule() { var e = new Error("Cannot find module \".\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()), function (i) {
+    return r('./middleware') + '/' + i;
+  }));
+};
 
 var start = function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_E_learn_vue_weixin_webapp_web_fire_node_modules_babel_runtime_regenerator___default.a.mark(function _callee2() {
-    var _this = this;
-
-    var app, host, port, config, nuxt, builder;
+    var config, nuxt;
     return __WEBPACK_IMPORTED_MODULE_0_E_learn_vue_weixin_webapp_web_fire_node_modules_babel_runtime_regenerator___default.a.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
-            app = new __WEBPACK_IMPORTED_MODULE_1_koa___default.a();
-            host = process.env.HOST || '127.0.0.1';
-            port = process.env.PORT || 3000;
-
-            // Import and Set Nuxt.js options
-
             config = __webpack_require__(0);
 
             config.dev = !(app.env === 'production');
+            _context2.next = 4;
+            return new __WEBPACK_IMPORTED_MODULE_4_nuxt___default.a(config);
 
-            // Instantiate nuxt.js
-            nuxt = new __WEBPACK_IMPORTED_MODULE_2_nuxt__["Nuxt"](config);
+          case 4:
+            nuxt = _context2.sent;
 
-            // Build in development
-
-            if (!config.dev) {
-              _context2.next = 10;
+            if (!(__WEBPACK_IMPORTED_MODULE_2__config__["a" /* default */].env !== 'production')) {
+              _context2.next = 15;
               break;
             }
 
-            builder = new __WEBPACK_IMPORTED_MODULE_2_nuxt__["Builder"](nuxt);
-            _context2.next = 10;
-            return builder.build();
+            _context2.prev = 6;
+            _context2.next = 9;
+            return nuxt.build();
 
-          case 10:
+          case 9:
+            _context2.next = 15;
+            break;
+
+          case 11:
+            _context2.prev = 11;
+            _context2.t0 = _context2['catch'](6);
+
+            console.error(_context2.t0);
+            process.exit(1);
+
+          case 15:
+            _context2.next = 17;
+            return useMiddleware(app)(MIDDLEWARE);
+
+          case 17:
 
             app.use(function () {
               var _ref2 = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_E_learn_vue_weixin_webapp_web_fire_node_modules_babel_runtime_regenerator___default.a.mark(function _callee(ctx, next) {
@@ -189,21 +299,11 @@ var start = function () {
                   while (1) {
                     switch (_context.prev = _context.next) {
                       case 0:
-                        _context.next = 2;
-                        return next();
+                        ctx.status = 200;
+                        _context.next = 3;
+                        return nuxt.render(ctx.req, ctx.res);
 
-                      case 2:
-                        ctx.status = 200; // koa defaults to 404 when it sees that status is unset
-                        return _context.abrupt('return', new Promise(function (resolve, reject) {
-                          ctx.res.on('close', resolve);
-                          ctx.res.on('finish', resolve);
-                          nuxt.render(ctx.req, ctx.res, function (promise) {
-                            // nuxt.render passes a rejected promise into callback on error.
-                            promise.then(resolve).catch(reject);
-                          });
-                        }));
-
-                      case 4:
+                      case 3:
                       case 'end':
                         return _context.stop();
                     }
@@ -216,15 +316,15 @@ var start = function () {
               };
             }());
 
-            app.listen(port, host);
-            console.log('Server listening on ' + host + ':' + port); // eslint-disable-line no-console
+            app.listen(__WEBPACK_IMPORTED_MODULE_2__config__["a" /* default */].port, __WEBPACK_IMPORTED_MODULE_2__config__["a" /* default */].host);
+            console.log('Server listening on ' + __WEBPACK_IMPORTED_MODULE_2__config__["a" /* default */].host + ':' + __WEBPACK_IMPORTED_MODULE_2__config__["a" /* default */].port); // eslint-disable-line no-console
 
-          case 13:
+          case 20:
           case 'end':
             return _context2.stop();
         }
       }
-    }, _callee2, this);
+    }, _callee2, _this, [[6, 11]]);
   }));
 
   return function start() {
@@ -232,12 +332,8 @@ var start = function () {
   };
 }();
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-
-
-
 start();
+/* WEBPACK VAR INJECTION */}.call(exports, "server"))
 
 /***/ }
 /******/ ]);
--- web/fire/server/index.js
+++ web/fire/server/index.js
@@ -1,39 +1,51 @@
 import Koa from 'koa'
-import { Nuxt, Builder } from 'nuxt'
+import conf from './config'
+import { resolve } from 'path'
+import Nuxt from 'nuxt'
+import nuxtConf from '../nuxt.config'
+// 函数式风格，数据不变性和函数无副作用是它的核心设计理念，结合它的函数自动柯里化可以让多个函数的排
+// 列和数据的变换传递变得更容易
+import R from 'ramda'
 
-async function start () {
-  const app = new Koa()
-  const host = process.env.HOST || '127.0.0.1'
-  const port = process.env.PORT || 3000
+const r = path => resolve(__dirname, path)
+const MIDDLEWARE = ['database', 'general', 'router']
+const app = new Koa()
 
-  // Import and Set Nuxt.js options
+const useMiddleware = (app) => {
+  // 中间件的个数不定，通过 Ramda 的特性，从右往左进行函数组合，右侧函数的返回结果总是左侧函数的输入参数
+  // R.map(console.log)([1, 2, 3])
+  // MIDDLEWARE 数组交给了 R.map
+  // 分别拿到的单个数组中的值，我们可以通过 R.compose 再次进行组合。
+  return R.map(R.compose(
+    R.map(i => i(app)),
+    require,
+    i => `${r('./middleware')}/${i}`)
+  )
+}
+
+const start = async () => {
   let config = require('../nuxt.config.js')
   config.dev = !(app.env === 'production')
+  const nuxt = await new Nuxt(config)
 
-  // Instantiate nuxt.js
-  const nuxt = new Nuxt(config)
-
-  // Build in development
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+  if (conf.env !== 'production') {
+    try {
+      await nuxt.build()
+    } catch (e) {
+      console.error(e)
+      process.exit(1)
+    }
   }
 
+  await useMiddleware(app)(MIDDLEWARE)
+
   app.use(async (ctx, next) => {
-    await next()
-    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-    return new Promise((resolve, reject) => {
-      ctx.res.on('close', resolve)
-      ctx.res.on('finish', resolve)
-      nuxt.render(ctx.req, ctx.res, promise => {
-        // nuxt.render passes a rejected promise into callback on error.
-        promise.then(resolve).catch(reject)
-      })
-    })
+    ctx.status = 200
+    await nuxt.render(ctx.req, ctx.res)
   })
 
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+  app.listen(conf.port, conf.host)
+  console.log('Server listening on ' + conf.host + ':' + conf.port) // eslint-disable-line no-console
 }
 
 start()
